f9fe045531c46512eb9e4883a1d7d631a0d0fa0f
racoon <racoon63@gmx.net>
2019-03-05 01:04:45 +0100

    added ssh key provision for ansible user

--- prepare-host.sh
+++ prepare-host.sh
@@ -3,18 +3,30 @@
 # Debugging option
 #set -x
 
-read -p    ">>> Enter IP address of target host: "                                 IP
-read -p    ">>> Enter SSH username with admin privileges (e.g. root): "            USERNAME
-read -s -p ">>> Enter password for ansible user: "                                 PASS
+KEYS=("id_dsa.pub" "id_ecdsa.pub" "id_ed25519.pub" "id_rsa.pub")
+KEYDIR="$HOME/.ssh"
+
+read -p    ">>> Enter IP address of target host: " IP
+read -p    ">>> Enter SSH username with admin privileges (e.g. root): " USERNAME
+read -s -p ">>> Enter password for ansible user: " PASS
 printf "\n"
-read -p    ">>> Do you want to provide a public SSH key for ansible user? [Y/n]: " SSH_KEY_QA
-read -e -p ">>> Enter absolute path to key: "                                      SSH_KEY_PATH
-PASS_ENCRYPTED=$(openssl passwd -crypt -salt racoon63 $PASS)
+read -p    ">>> Do you want to provide your public SSH key for ansible user? [Y/n]: " SSH_KEY_QA
+
+for KEY in "${KEYS[@]}"
+do
+    if [ -e $KEYDIR/$KEY ]
+    then
+        SSH_KEY_PATH="$KEYDIR/$KEY"
+        break
+    fi
+done
+
 SSH_KEY=$(cat $SSH_KEY_PATH)
+PASS_ENCRYPTED=$(openssl passwd -crypt -salt racoon63 $PASS)
 
 printf ">>> Prepare target host: $IP \n"
 
-ssh $USERNAME@$IP <<EOF
+ssh $USERNAME@$IP <<EOF &>/dev/null
 $(typeset -f $PASS_ENCRYPTED $SSH_KEY)
 adduser --base-dir /home/ --home-dir /home/ansible --groups wheel --password $PASS_ENCRYPTED ansible
 echo 'ansible ALL=(ALL) NOPASSWD:ALL' | tee -a /etc/sudoers &>/dev/null
